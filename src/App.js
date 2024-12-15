322b0fc1f1983b22176ddcf178af24c4a977850a
Farhan Nawaz <farhannawazofficial@gmail.com>
2024-12-15 15:45:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 
+
 function App() {
   return (
     <div className="App">
+      <h1>
+        hlo
+      </h1>
       <Navbar />
     </div>
   );
